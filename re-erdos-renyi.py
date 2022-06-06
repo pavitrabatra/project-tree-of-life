291fb9ff99e4eef1db6b7f27234315873fb69d85
pavitrabatra <pavitrabatra@gmail.com>
2022-06-06 12:53:01 +0530

    Multiple Beta

--- re-erdos-renyi.py
+++ re-erdos-renyi.py
@@ -5,27 +5,35 @@ from scipy.linalg import expm
 
 #For non sparse graph; faster algorithm exists for sparse(small p) graphs
 G = nx.gnp_random_graph(200, 0.5, seed=None, directed=False)
-nx.draw(G, with_labels=True)
+#nx.draw(G, with_labels=True)
 
 beta = 1/10000
+x=np.empty(9)
+y=np.empty(9)
 
-L = nx.laplacian_matrix(G)
-#The above generates a scipy sparse array and not a matrix(Better for comutational purposes?);
+while (beta <= 10000):
 
-#Converting the sparse array to matrix 
-L = L.todense()
+    L = nx.laplacian_matrix(G)
+    #The above generates a scipy sparse array and not a matrix(Better for comutational purposes?);
 
-pho_tmp = expm(-beta*L)
+    #Converting the sparse array to matrix 
+    L = L.todense()
 
-Z = pho_tmp.trace()
+    pho_tmp = expm(-beta*L)
+    Z = pho_tmp.trace()
+    pho = expm(-beta*L)/Z
+    Lp = np.matmul(L,pho)
+    Tr = Lp.trace()
 
-pho = expm(-beta*L)/Z
+    S = np.log2(Z) + (beta/(np.log(2)) * Tr)
+    print(S/(np.log2(200)))
+   
+    x = np.append(x, 1/beta)
+    y = np.append(y, S/(np.log2(200)))
 
-Lp = np.matmul(L,pho)
+    beta = beta *10
 
-Tr = Lp.trace()
-
-S = np.log2(Z) + (beta/(np.log(2)) * Tr)
-
-print(S/(np.log(200)))
+print(x)
+print(y)
+plt.plot(x,y)
 plt.show()
