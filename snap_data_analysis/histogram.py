7bfc3950e4f83f795f1c3cc21b6e9beb7c24a1a7
pavitrabatra <pavitrabatra@gmail.com>
2022-09-14 04:31:53 +0530

    git ignore file added

--- snap_data_analysis/histogram.py
+++
@@ -1,8 +0,0 @@
-import matplotlib.pyplot as plt
-import numpy as np
-
-data = np.loadtxt("size.txt", usecols=(1,), unpack=True)
-plt.hist(data, bins=100)
-plt.ylabel('Frequency')
-plt.xlabel('Network Size')
-plt.show()
